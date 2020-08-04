b8405803ca4a731a527006ac5cff38c08c5a2bc1
vladislavLiesiechko <36047217+vladislavLiesiechko@users.noreply.github.com>
2020-08-05 02:04:09 +0300

    Update 11.js

--- 11.js
+++ 11.js
@@ -4,5 +4,12 @@ let num=Math.floor(Math.random() * 11);
 console.log(num);
 let userNumber;
 do {
-    userNumber = +prompt("Введите число от 0 до 10" );
+  userNumber = prompt("Введите число от 0 до 10" );
+  if ( Number(userNumber) === num){
+    alert("Вы выиграли");
+  }
+  else if( userNumber === null){
+    alert("Вы вышли из игры");
+  }
 } while (userNumber!==num);
+
