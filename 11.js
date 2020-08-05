37be63a5202621293607d697dee6725e112475e9
vladislavLiesiechko <36047217+vladislavLiesiechko@users.noreply.github.com>
2020-08-05 14:39:08 +0300

    Update 11.js

--- 11.js
+++ 11.js
@@ -1,15 +1,17 @@
 "use strict"
 
-let num = Math.floor(Math.random() * 11);
+llet num=Math.floor(Math.random() * 11);
 console.log(num);
 let userNumber;
 do {
   userNumber = prompt("Введите число от 0 до 10" );
   if ( Number(userNumber) === num){
     alert("Вы выиграли");
+
   }
-  else if( userNumber === null){
+  else if( userNumber === null ){
     alert("Вы вышли из игры");
+    break;
   }
-} while (userNumber!==num);
+} while (Number(userNumber)!==num);
 
